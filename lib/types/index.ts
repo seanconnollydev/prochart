6774a05e89f0fd5dae3d8597a7ac0798eb7dc92f
Sean Connolly <sean@seanconnolly.dev>
2026-07-05 06:25:06 -0600

    Add simulation structure

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,5 +1,6 @@
 export * from "./case-study";
 export * from "./assessment-template";
 export * from "./assessment-submission";
+export * from "./simulation-template";
 export * from "./prompt-patch";
 export * from "./local-meta";
